3a36cb78965ae469ad256ed070af8e4160d30d2f
igor.vlahek <igor.vlahek@intis.hr>
2020-06-14 21:56:32 +0200

    repository interceptor  updated

--- multitenant-showcase-service/src/main/java/hr/ivlahek/showcase/aop/RepositoryInterceptor.java
+++ multitenant-showcase-service/src/main/java/hr/ivlahek/showcase/aop/RepositoryInterceptor.java
@@ -7,6 +7,7 @@ import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.persistence.EntityManager;
@@ -20,13 +21,26 @@ public class RepositoryInterceptor {
 
     private Logger logger = LoggerFactory.getLogger(RepositoryInterceptor.class);
 
+    @Value("${spring.profiles.active:prod}")
+    private String activeProfile;
+
     @Around("execution(* org.springframework.data.repository.Repository+.*(..))")
     public Object inWebLayer(ProceedingJoinPoint joinPoint) throws Throwable {
-        if (entityManager.isJoinedToTransaction()) {
-            Session session = entityManager.unwrap(Session.class);
-            Integer id = TenantContext.getCurrentTenant().getId();
-            session.enableFilter("userAccountFilter").setParameter("tenantId", id);
-            session.enableFilter("mobileApplicationFilter").setParameter("tenantId", id);
+
+        if (activeProfile.equals("prod")) {
+            if (entityManager.isOpen()) {
+                Session session = entityManager.unwrap(Session.class);
+                Integer id = TenantContext.getCurrentTenant().getId();
+                session.enableFilter("userAccountFilter").setParameter("tenantId", id);
+                session.enableFilter("mobileApplicationFilter").setParameter("tenantId", id);
+            }
+        } else {
+            if (entityManager.isJoinedToTransaction()) {
+                Session session = entityManager.unwrap(Session.class);
+                Integer id = TenantContext.getCurrentTenant().getId();
+                session.enableFilter("userAccountFilter").setParameter("tenantId", id);
+                session.enableFilter("mobileApplicationFilter").setParameter("tenantId", id);
+            }
         }
         return joinPoint.proceed();
     }
